8130dbbdd7b120e9acdf7587aae0ac7b9261a1d5
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2024-08-23 15:02:03 +0200

    Don't update LD_LIBRARY_PATH globally

--- smtpd/images/centos-7/entrypoint.sh
+++ smtpd/images/centos-7/entrypoint.sh
@@ -2,4 +2,4 @@
 
 /opt/halon/bin/halonctl license fetch --username ${HALON_REPO_USER} --password ${HALON_REPO_PASS} --path /license.key
 
-exec "$@"
+exec env LD_LIBRARY_PATH=/opt/halon/lib/:$LD_LIBRARY_PATH "$@"
