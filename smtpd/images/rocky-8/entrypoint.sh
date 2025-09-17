432fb1db4741c407a54e73ba7aa6aa6248d2ab27
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2025-09-17 13:48:56 +0000

    Bump smtpd and api versions

--- smtpd/images/rocky-8/entrypoint.sh
+++ smtpd/images/rocky-8/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 /opt/halon/bin/halonctl license fetch --username ${HALON_REPO_USER} --password ${HALON_REPO_PASS} --path /license.key
 
-exec env LD_LIBRARY_PATH=/opt/halon/lib/:$LD_LIBRARY_PATH "$@"
+exec "$@"
